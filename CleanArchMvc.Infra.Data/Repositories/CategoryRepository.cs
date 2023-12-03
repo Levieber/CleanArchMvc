499e7212bdbd2ab3889b88f612479e29b8dda8df
Levi Eber <gblevieber@gmail.com>
2023-12-03 20:10:26 -0300

    feat: implement infra data and ioc

---
+++ CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    ApplicationDbContext _context;
+
+    public CategoryRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Category> CreateAsync(Category category)
+    {
+        _context.Add(category);
+        await _context.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<Category> GetByIdAsync(int? id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+        return category;
+    }
+
+    public async Task<IEnumerable<Category>> GetCategoriesAsync()
+    {
+        var categories = await _context.Categories.ToListAsync();
+        return categories;
+    }
+
+    public async Task<Category> RemoveAsync(Category category)
+    {
+        _context.Remove(category);
+        await _context.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<Category> UpdateAsync(Category category)
+    {
+        _context.Update(category);
+        await _context.SaveChangesAsync();
+        return category;
+    }
+}
---
+++ CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    ApplicationDbContext _context;
+
+    public ProductRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Product> CreateAsync(Product product)
+    {
+        _context.Add(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<Product> GetByIdAsync(int? id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        return product;
+    }
+
+    public async Task<Product> GetProductCategoryAsync(int? id)
+    {
+        var product = await _context.Products.Include(p => p.Category).SingleOrDefaultAsync(p => p.Id == id);
+        return product;
+    }
+
+    public async Task<IEnumerable<Product>> GetProductsAsync()
+    {
+        var products = await _context.Products.ToListAsync();
+        return products;
+    }
+
+    public async Task<Product> RemoveAsync(Product product)
+    {
+        _context.Remove(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+
+    public async Task<Product> UpdateAsync(Product product)
+    {
+        _context.Update(product);
+        await _context.SaveChangesAsync();
+        return product;
+    }
+}
